672a309ef42e8679dfb260889d46d550fe914c41
Anthony Doran <tony@simplyput.ai>
2023-08-22 13:52:53 -0700

    added in first test

--- questions.py
+++ questions.py
@@ -2,6 +2,5 @@ from sp import hasSql, inSpace
 
 # Here is where you put in the questions you want to test:
 questions = [
-    # This is is how you would check the arr question has sql, and is in the "Metrics" space
-    # ("What is our arr?", [hasSql, inSpace('Metrics')])
+    ("How many questions were there?", [hasSql, inSpace("Questions")])
 ]
