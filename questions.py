f98147d128936d8934e0702254deb150e97f4f5a
Anthony Doran <tony@simplyput.ai>
2023-08-22 14:15:57 -0700

    hmmm

--- questions.py
+++ questions.py
@@ -1,5 +1,6 @@
 from sp import hasSql, inSpace
-
+import os
+print(os.getenv("ORG_ID"))
 # Here is where you put in the questions you want to test:
 questions = [
     ("How many questions were there?", [hasSql, inSpace("Questions")])
